0c9671f13d928d8ddc32b8c44c8c1de17050345a
Randy Do <randyydoo@csu.fullerton.edu>
2023-04-10 19:37:10 -0700

    list of dics

--- wiki.py
+++ wiki.py
@@ -3,19 +3,30 @@ import wikipediaapi
 import random
 
 articles = ["Basketball", "Soccer", "Football" , "Tennis", "Baseball", "Golf"]
+valid = []
+#insert all subaritlces into valid
+for section in articles:
+    dic = {}
+    dic["Sport"] = section
+    wiki = wikipediaapi.Wikipedia('en')
+    page = wiki.page(section)
+    for subsection in page.sections:
+        if subsection.title in ["See also", "References", "Further Reading", "External links" ]:
+            continue
+        if len(subsection.text) >= 35:
+            dic["Subsection"] = subsection.title
+            dic["Text"] = subsection.text
+            valid.append(dic)
+print(len(valid))
+def get_title():
+    return
+def get_sub_article():
+    return
+def get_text():
+    return
+def get_random_article_index():
+    num =  random.randint(0,len(valid) - 1)
+    return num
 
-#find random article
-def get_random():
-    return random.randint(0,len(articles) - 1)
-def get_subtitles():
-    valid = []
-    for article in articles:
-        wiki = wikipediaapi.Wikipedia('en')
-        page = wiki.page(article)
-        for subsection in page.sections:
-            if subsection.title in ["See also", "References", "Further Reading", "External links" ]:
-                continue
-            if len(subsection.text) >= 35:
-                valid.append(subsection.text)
-
-
+def get_len(subsec):
+    return len(subsec)
