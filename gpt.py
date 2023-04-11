d8c4792b51600015a84bf642733becb202f72519
Randy Do <randyydoo@csu.fullerton.edu>
2023-04-11 15:49:19 -0700

    changed fucntion names

--- gpt.py
+++ gpt.py
@@ -1,7 +1,7 @@
 import openai
+import random 
 import os
-import wiki
-import data
+import data 
 list1 = []
 def get_articles():
     api_prompt = """For an article about "ARTICLE", make a section titled "HEADER" that is about COUNT characters long. The section is a part of a larger article about "ARTICLE".  Send only the section and omit the section's header and try to keep it in one paragraph. Write it in the style of an encyclopedia."""
@@ -16,12 +16,19 @@ def get_articles():
         res = openai.Completion.create(engine = "text-davinci-001", prompt = api_prompt, max_tokens = 1024)
         text = res.choices[0].text
         text = text.replace("\n", "")
-        if text[0] == "?":
-            text = text.replace("?" , "")
         dic["Sport"] = article["Sport"]
         dic["Subsection"] = article["Subsection"]
         dic["Text"] = text
         list1.append(dic)
-        print(list1[0]["Text"])
-        return
-get_articles()
+
+def gpt_sport():
+    return data.gpt[get_random()]["Sport"]
+
+def gpt_sub_article():
+    return data.gpt[get_random()]["Subsection"]
+
+def gpt_text():
+    return data.gpt[get_random()]["Text"]
+
+def gpt_len()
+    return len(data.gpt[num]["Text"])
--- wiki.py
+++ wiki.py
@@ -1,32 +1,36 @@
 #this file is used to get subsections from an article
 import wikipediaapi
-import random
+import random 
+import data
+import app
 
 articles = ["Basketball", "Soccer", "Football" , "Tennis", "Baseball", "Golf"]
-valid = []
 #insert all subaritlces into valid
-for section in articles:
-    dic = {}
-    dic["Sport"] = section
-    wiki = wikipediaapi.Wikipedia('en')
-    page = wiki.page(section)
-    for subsection in page.sections:
-        if subsection.title in ["See also", "References", "Further Reading", "External links" ]:
-            continue
-        if len(subsection.text) >= 35:
-            dic["Subsection"] = subsection.title
-            dic["Text"] = subsection.text
-            valid.append(dic)
-print(len(valid))
-def get_title():
-    return
-def get_sub_article():
-    return
-def get_text():
-    return
-def get_random_article_index():
-    num =  random.randint(0,len(valid) - 1)
-    return num
+def get_list():
+    valid = []
+    for section in articles:
+        dic = {}
+        dic["Sport"] = section
+        wiki = wikipediaapi.Wikipedia('en')
+        page = wiki.page(section)
+        for subsection in page.sections:
+            if subsection.title in ["See also", "References", "Further Reading", "External links" ]:
+                continue
+            if len(subsection.text) >= 35:
+                dic["Subsection"] = subsection.title
+                dic["Text"] = subsection.text
+                valid.append(dic)
+    return valid
+#get random value from list
 
-def get_len(subsec):
-    return len(subsec)
+def wiki_get_sport():
+    return data.wiki[app]["Sport"]
+
+def wiki_sub_article():
+    return data.wiki[get_random()]["Subsection"]
+
+def wiki_text():
+    return data.wiki[get_random()]["Text"]
+
+def wiki_len(text):
+    return len(text)
