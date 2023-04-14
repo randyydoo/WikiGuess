359912ea3a04e33c72f79c731aa40d72a2a49f38
Randy Do <randyydoo@csu.fullerton.edu>
2023-04-14 14:57:44 -0700

    fixed styles and alg

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import time
 
 app = Flask(__name__) 
 output = []
+score = 0
 highScore = 0
 
 #use index.html as loading page and give out directions with a coutndown
@@ -17,20 +18,22 @@ def index():
 
 @app.route('/start', methods= ['POST', 'GET'])
 def start():
-    if request.method == 'POST':
+    num = random.randint(0, len(data.wiki) - 1)
+    while num in output or "ISBN" in data.wiki[num]["Text"]:
         num = random.randint(0, len(data.wiki) - 1)
-        while num in output or "ISBN" in data.wiki[num]["Text"]:
-            num = random.randint(0, len(data.wiki) - 1)
-
-        output.append(num)
-        
+        #keep track of used text
+    output.append(num)
+    if request.method == 'POST':
         wiki_sport = data.wiki[num]["Sport"]
         wiki_article = data.wiki[num]["Text"]
 
         gpt_sport = data.gpt[num]["Sport"]
         gpt_article = data.gpt[num]["Text"]
-        return render_template('base.html' , wiki_sport = wiki_sport, wiki_article = wiki_article, gpt_sport = gpt_sport, gpt_article = gpt)
-
+        return render_template('base.html' , score = score, wiki_sport = wiki_sport, wiki_article = wiki_article, gpt_sport = gpt_sport, gpt_article = gpt_article)
+    #handle false https request
+    else:
+        return redirect("/")
+       
 #use game.html to load game with screen
 #if correct guess render 'win.html' , update Title, Subsection, Text, rest for 1.5 sec, return render_template  'base.html'
 #else render 'lose.html', rest for 1.5 sec, return render_template 'base.html'
