b042b4071342196fffc153fcdd78b7fcfce8a7a0
Randy Do <randyydoo@csu.fullerton.edu>
2023-04-19 09:55:59 -0700

    loads texts

--- static/main.js
+++ static/main.js
@@ -1,50 +1,38 @@
 var score = 0;
+var g_json
 
-document.getElementById("start").addEventListener("click", function() {
-  fetch("/get_text")
-    .then(response => response.json())
-    .then(data => {
-      newText(data);
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+function start() {
+  return fetch('/get_text')
+    .then(response => {
+    console.log(response);
+      return response.json();
     })
-    .catch(error => console.error(error));
-});
-
-document.getElementById('left_button').addEventListener('click', function() {
-  var value = document.getElementById('left_button').value;
-  if (value === 'gpt') {
-    score++;
-  } else {
-    score = 0;
-  }
-  document.getElementById('score').innerHTML = 'Score: ' + score;
-});
-
-document.getElementById('right_button').addEventListener('click', function() {
-  var value = document.getElementById('right_button').value;
-  if (value === 'gpt') {
-    score++;
-  } else {
-    score = 0;
-  }
-  document.getElementById('score').innerHTML = 'Score: ' + score;
-});
+    .then(data => {
+      console.log(data);
+      return data;
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
 
 // Function to set text for left and right buttons
 function newText(data) {
+  var random = getRandomInt(0,2);
   var wiki_text = data['Wiki'];
   var gpt_text = data['Gpt'];
   var sport = data['Sport'];
-  var random = data["Random"]
   
   let left = document.getElementById('left_button');
   let right = document.getElementById('right_button');
   let left_sport = document.getElementById('left_sport');
   let right_sport = document.getElementById('right_sport');
-  let score = document.getElementById('score');
 
   left.style.display = "block";
   right.style.display = "block";
-  score.innerHTML = 'Score: ' + score;
   
   if (random == 1) {
     left.innerHTML = wiki_text;
@@ -53,17 +41,72 @@ function newText(data) {
 
     right.innerHTML = gpt_text;
     right.value = 'gpt';
-    right.innerHTML = sport;
-  } else {
+    right_sport.innerHTML = sport;
+  } 
+  else {
     right.innerHTML = wiki_text;
     right.value = 'wiki';
     right_sport.innerHTML = sport;
-
+  
     left.innerHTML = gpt_text;
     left.value = 'gpt';
-    left.innerHTML = sport;
+    left_sport.innerHTML = sport;
   }
 }
+
+
+document.getElementById("start").addEventListener("click", function() {
+  start().then(data => {
+    g_json = data;
+    newText(data);
+}).catch(error => {
+  console.error(error);
+});
+});
+
+document.getElementById('left_button').addEventListener('click', function() {
+  var value = document.getElementById('left_button').value;
+  if (value === 'gpt') {
+    score++;
+    start().then(data => {
+      g_json = data;
+      newText(data);
+  }).catch(error => {
+    console.error(error);
+  });
+  } else {
+    score = 0;
+    start().then(data => {
+      g_json = data;
+      newText(data);
+  }).catch(error => {
+    console.error(error);
+  });
+  }
+  document.getElementById('score').innerHTML = 'Score: ' + score;
+});
+
+document.getElementById('right_button').addEventListener('click', function() {
+  var value = document.getElementById('right_button').value;
+  if (value === 'gpt') {
+    score++;
+    start().then(data => {
+      g_json =data;
+      newText(data);
+  }).catch(error => {
+    console.error(error);
+  });
+  } else {
+    score = 0;
+    start().then(data => {
+      g_json = data;
+      newText(data);
+  }).catch(error => {
+    console.error(error);
+  });
+  }
+  document.getElementById('score').innerHTML = 'Score: ' + score;
+});
 
 
 
